c9f6a2a997091d683f97c14a2f2a611e0f203f30
machao <490521687@qq.com>
2016-01-10 22:00:33 +0800

    how to write a singleton

---
+++ MySwiftPlayground/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MySwiftPlayground
+//
+//  Created by Erickson on 16/1/10.
+//  Copyright © 2016年 erickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+        
+    }
+
+
+}
+
+
+/// the right way to write a singleton
+class TheOneAndOnly{
+    static let sharedInstance = TheOneAndOnly()
+    private init() {} //This prevents others from using the default '()' initializer for this class.
+}
