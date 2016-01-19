42f99962dbc9562c49e1e570988d84e39049a1e3
machao <490521687@qq.com>
2016-01-19 20:15:15 +0800

    deve

--- MySwiftPlayground/MyPlayground.playground/Contents.swift
+++ MySwiftPlayground/MyPlayground.playground/Contents.swift
@@ -18,6 +18,11 @@ let digitNames = [
     5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
 ]
 
+let digitNames2 = [
+    0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+
 
 let numbers = [16, 58, 510]
 
